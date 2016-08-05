d0990558ed594c4b61e335bf1f7cecc72e82faee
Miroslav Simko <mirsimko@gmail.com>
2016-08-05 02:11:56 -0700

    small changes

--- SubmitDrawMass.sh
+++ SubmitDrawMass.sh
@@ -2,10 +2,16 @@
 
 prodPath=/global/project/projectdirs/star/pwg/starhf/simkomir/LambdaC/histProd1
 
-
-mkdir -d $prodPath
+mkdir -p $prodPath
 cp SubmitDrawMass.xml DrawMass.C $prodPath
 
 pushd $prodPath >> /dev/null
+if [ -e LocalLibraries.zip ]; then
+  rm LocalLibraries.zip
+fi
+
+if [ -d LocalLibraries.package ]; then
+  rm -rf LocalLibraries.package
+fi 
 star-submit SubmitDrawMass.xml
 popd >> /dev/null
