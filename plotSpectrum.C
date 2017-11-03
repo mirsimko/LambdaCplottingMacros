bd2a8b58f8a18a9cf4a710dcdf56268a8fd5c401
Miroslav Simko <mirsimko@gmail.com>
2017-11-03 18:19:39 -0400

    cleaned the code from most code duplication

--- plotSpectrum.C
+++ plotSpectrum.C
@@ -26,32 +26,48 @@
 using std::cout;
 using std::endl;
 
-// -- binning
-const int nBinSixty = 1;
-float ptBinSixty[nBinSixty+1] = {3., 6.};
-float nSixty[nBinSixty] = {81.7087};
-float errorSixty[nBinSixty] = {15.8583};
-float xSixty[nBinSixty] = {4.5};
-float xEsixty[nBinSixty] = {1.5};
+float addErrors(float first, float second);
 
-const int nBinEighty = 2;
-float ptBinEighty[nBinEighty+1] = {3., 4., 6.};
-float nEighty[nBinEighty] = {73.3045, 19.8597};
-float errorEighty[nBinEighty] = {14.921, 6.47855};
-float xEighty[nBinEighty] = {3.5, 5.};
-float xEeighty[nBinEighty] = {0.5, 1.};
+// -- namespace with consts
+namespace LCyieldsNov2017
+{
+  // -- consts
+  const int nBinSixty = 1;
+  float ptBinSixty[nBinSixty+1] = {3., 6.};
+  float nSixty[nBinSixty] = {81.7087};
+  float errorSixty[nBinSixty] = {15.8583};
+  float xSixty[nBinSixty] = {4.5};
+  float xEsixty[nBinSixty] = {1.5};
 
-const float BR = 0.0635;
-const float nEvents = 9.75433e+08;
+  const int nBinEighty = 2;
+  float ptBinEighty[nBinEighty+1] = {3., 4., 6.};
+  float nEighty[nBinEighty] = {73.3045, 19.8597};
+  float errorEighty[nBinEighty] = {14.921, 6.47855};
+  float xEighty[nBinEighty] = {3.5, 5.};
+  float xEeighty[nBinEighty] = {0.5, 1.};
 
-const float firstCent = 0;
-const float lastCent = 6;
+  const float BR = 0.0635;
+  const float nEvents = 9.75433e+08;
+
+  const int nCharges = 2;
+  const float fracEventsSixty = 0.5;
+  const float fracEventsEighty = 0.7;
+  const float deltaRapidity = 2.;
+
+  const float firstCent = 0;
+  const float lastCent = 6;
 
 //--------------------------------------------------
-float addErrors(float first, float second)
-{
-  return sqrt(first*first + second*second);
+  double scalingFactor(int iBin, TGraph* eff, float pt, float deltaPt)
+  {
+    double effX, effY;
+    eff->GetPoint(iBin, effX, effY);
+    const double scaling= 1./(2.*TMath::Pi()*nCharges*BR*deltaRapidity*pt*nEvents*fracEventsSixty*deltaPt*effY);
+    return scaling;
+  }
+//--------------------------------------------------
 }
+
 //--------------------------------------------------
 
 void plotSpectrum()
@@ -64,54 +80,54 @@ void plotSpectrum()
   gStyle->SetTitleFont(132, "y");
   gStyle->SetLegendFont(132);
 
-  string inFileNameSixty = "efficiency_result_10-60cent.root";
+  using namespace LCyieldsNov2017;
+
+  string inFileNameSixty = "images/efficiency_result_10-60cent.root";
   TFile *inFileSixty = new TFile(inFileNameSixty.c_str());
   TGraphAsymmErrors* effSixty = static_cast<TGraphAsymmErrors*>(inFileSixty->Get("Graph;1"));
-  string inFileNameEighty = "efficiency_result10-80cent.root";
+  string inFileNameEighty = "images/efficiency_result10-80cent.root";
   TFile *inFileEighty = new TFile(inFileNameEighty.c_str());
   TGraphAsymmErrors* effEighty = static_cast<TGraphAsymmErrors*>(inFileEighty->Get("Graph;1"));
 
   string outFileName = "spectrum.root";
   TFile *outFile = new TFile(outFileName.c_str(), "recreate");
 
-  TCanvas *Csixty = new TCanvas("Csixty", "10-60\% centrality", 400, 600);
-  TCanvas *Ceighty = new TCanvas("Ceighty", "10-80\% centrality", 400, 600);
+  TCanvas *Csixty = new TCanvas("Csixty", "10-60\% centrality", 200, 300);
+  TCanvas *Ceighty = new TCanvas("Ceighty", "10-80\% centrality", 200, 300);
 
-  double scalingFactorSixty[nBinSixty];
-  double scalingFactorEighty[nBinEighty];
- 
   // correction for sixty
   for (int iBin = 0; iBin < nBinSixty; ++iBin)
   {
-    double effX, effY;
-    effSixty->GetPoint(iBin, effX, effY);
-    scalingFactorSixty[iBin]= 1./(2*TMath::Pi()*2*BR*2.*xSixty[iBin]*nEvents*.5*xEsixty[iBin]*effY);
-    cout << scalingFactorSixty[iBin] << endl;
+    const double correction = scalingFactor(iBin, effSixty, xSixty[iBin], xEsixty[iBin]);
 
-    nSixty[iBin] = nSixty[iBin]*scalingFactorSixty[iBin];
-    errorSixty[iBin] = errorSixty[iBin]*scalingFactorSixty[iBin];
+    nSixty[iBin] *= correction;
+    errorSixty[iBin] *= correction;
   }
 
   // correction for eighty
   for (int iBin = 0; iBin < nBinEighty; ++iBin)
   {
-    double effX, effY;
-    effEighty->GetPoint(iBin, effX, effY);
-    scalingFactorEighty[iBin]= 1./(2*TMath::Pi()*2*BR*2.*xEighty[iBin]*nEvents*.7*xEeighty[iBin]*effY);
+    const double correction = scalingFactor(iBin, effEighty, xEighty[iBin], xEeighty[iBin]);
 
-    cout << scalingFactorEighty[iBin] << endl;
-    nEighty[iBin] *= scalingFactorEighty[iBin];
-    errorEighty[iBin] *= scalingFactorEighty[iBin];
+    nEighty[iBin] *= correction;
+    errorEighty[iBin] *= correction;
   }
   // -- Make TGraphErrors
   TGraphErrors* gSixty = new TGraphErrors(nBinSixty, xSixty, nSixty, xEsixty, errorSixty);
   TGraphErrors* gEighty = new TGraphErrors(nBinEighty, xEighty, nEighty, xEeighty, errorEighty);
 
   // -- Drawing
-  gSixty->GetXaxis()->SetTitle("#font[12]{p}_{T} (GeV/#font[12]{c})");
-  gSixty->GetXaxis()->SetRangeUser(0, 6);
-  gSixty->GetYaxis()->SetRangeUser(3e-6, 6e-4);
+  gSixty ->GetXaxis()->SetTitle("#font[12]{p}_{T} (GeV/#font[12]{c})");
+  gSixty ->GetYaxis()->SetTitle("#frac{d^{2}#font[12]{N}}{d#font[12]{p}_{T}d#font[12]{y}}#frac{1}{2#pi^{}#font[12]{p}_{T}}");
+  gSixty ->GetXaxis()->CenterTitle();
+  gSixty ->GetYaxis()->CenterTitle();
+  gSixty ->GetXaxis()->SetRangeUser(0, 6);
+  gSixty ->GetYaxis()->SetRangeUser(3e-6, 6e-4);
+
   gEighty->GetXaxis()->SetTitle("#font[12]{p}_{T} (GeV/#font[12]{c})");
+  gEighty->GetYaxis()->CenterTitle();
+  gEighty->GetXaxis()->CenterTitle();
+  gEighty->GetYaxis()->SetTitle("#frac{d^{2}#font[12]{N}}{d#font[12]{p}_{T}d#font[12]{y}}#frac{1}{2#pi^{}#font[12]{p}_{T}}");
   gEighty->GetXaxis()->SetRangeUser(0, 6);
   gEighty->GetYaxis()->SetRangeUser(3e-6, 6e-4);
 
@@ -125,9 +141,14 @@ void plotSpectrum()
   gEighty->Draw("ap");
 
   // saving
-  cout << "Writing into file " << outFileName << endl;
-  outFile->cd();
-  gSixty->Write();
-  gEighty->Write();
+  // cout << "Writing into file " << outFileName << endl;
+  // outFile->cd();
+  // gSixty->Write();
+  // gEighty->Write();
 }
 
+//--------------------------------------------------
+float addErrors(float first, float second)
+{
+  return sqrt(first*first + second*second);
+}
