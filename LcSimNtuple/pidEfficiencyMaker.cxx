30b7c3627571fd64a456a603292503d4134ada6e
Miroslav Simko <mirsimko@gmail.com>
2018-02-07 19:44:31 -0500

    Init() implemented

--- LcSimNtuple/pidEfficiencyMaker.cxx
+++ LcSimNtuple/pidEfficiencyMaker.cxx
@@ -3,15 +3,28 @@
 #include <TF1.h>
 #include <array>
 #include <TGraphErrors.h>
+#include <string>
+#include <TString.h>
 
 class pidEfficiencyMaker;
 
 
 // default constructor
-pidEfficiencyMaker::pidEfficiencyMaker():
-  simTuple(nullptr),
-  fHftRatioCorrection(nullptr),
-  gf1AuAu010Weight(nullptr)
+pidEfficiencyMaker::pidEfficiencyMaker(std::string outFileName, int centrality):
+  simTuple            (nullptr),
+  fHftRatioCorrection (nullptr),
+  gf1AuAu010Weight    (nullptr),
+  hNoCuts             (nullptr), 
+  hNoCutsPhysBinning  (nullptr),
+  hTopoCuts           (nullptr),
+  hHftMatchingOnly    (nullptr),
+  hPIDOnly            (nullptr),
+  hTpcOnly            (nullptr),
+  hTpcHftTopo         (nullptr),
+  h2MassPt            (nullptr),
+  mCentrality         (centrality),
+  mMinPtCut           (-1.),
+  outFile             (nullptr)
 {
 
   for( int i = 0; i < 3; ++i)
@@ -21,9 +34,62 @@ pidEfficiencyMaker::pidEfficiencyMaker():
     for (int j = 0; j < NptBins; ++j)
       ftof[j][i] = nullptr;
   }
+
+  outFile = new TFile(outFile.data(), "RECREATE");
 }
 
 ~pidEfficiencyMaker()
-{}
+{
+  // the pointers to the distributions are destroyed automatically when closing
+  // the file
+}
+
+void pidEfficiencyMaker::Init()
+{
+  int nBins = 300;
+  float minPt = 0.;
+  float maxPt = 15.;
+
+  float minPtCut;
+
+  if (mMinPtCut <= 0;)
+    minPtCut = 0;
+  else
+    minPtCut = mMinPtCut;
+
+  hNoCuts = new TH1D(Form("hNoCuts_minPt%i_%i", (int)(minPtCut * 1e3), mCentrality), Form("No Cuts %s minPt>%1.2f", anaCuts::physCentralityName[mCentrality].Data(), minPtCut), nBins, minPt, maxPt);
+  hNoCutsPhysBinning = new TH1D(Form("hNoCutsPhysBinning_minPt%i_%i", (int)(minPtCut * 1e3), mCentrality), Form("No Cuts Physics Binning %s minPt>%1.2f", anaCuts::physCentralityName[mCentrality].Data(), minPtCut), anaCuts::physNPtBins, anaCuts::physPtEdge);
+  hTopoCuts = new TH1D(Form("hTopoCuts_minPt%i_%i", (int)(minPtCut * 1e3), mCentrality), Form("Topo Cuts %s minPt>%1.2f", anaCuts::physCentralityName[mCentrality].Data(), minPtCut), nBins, minPt, maxPt);
+  hHftMatchingOnly = new TH1D(Form("hHftMatchingOnly_minPt%i_%i", (int)(minPtCut * 1e3), mCentrality), Form("HFT Matching Only %s minPt>%1.2f", anaCuts::physCentralityName[mCentrality].Data(), minPtCut), nBins, minPt, maxPt);
+  hPIDOnly= new TH1D(Form("hPIDOnly_minPt%i_%i", (int)(minPtCut * 1e3), mCentrality), Form("PID Only %s minPt>%1.2f", anaCuts::physCentralityName[mCentrality].Data(), minPtCut), nBins, minPt, maxPt);
+  hTpcOnly = new TH1D(Form("hTpcOnly_minPt%i_%i", (int)(minPtCut * 1e3), mCentrality), Form("TPC Only %s minPt>%1.2f", anaCuts::physCentralityName[mCentrality].Data(), minPtCut), nBins, minPt, maxPt);
+  hTpcHftTopo = new TH1D(Form("hTpcHftTopo_minPt%i_%i", (int)(minPtCut * 1e3), mCentrality), Form("TPC + HFT + Topo %s minPt>%1.2f", anaCuts::physCentralityName[mCentrality].Data(), minPtCut), nBins, minPt, maxPt);
+  h2MassPt = new TH2D(Form("h2MassPt_minPt%i_%i", (int)(minPtCut * 1e3), mCentrality), Form("Invariant Mass vs. Pt %s minPt>%1.2f", anaCuts::physCentralityName[mCentrality].Data(), minPtCut), nBins, minPt, maxPt, 210, 0, 2.1);
+
+  hNoCuts->Sumw2();
+  hNoCutsPhysBinning->Sumw2();
+  hTopoCuts->Sumw2();
+  hHftMatchingOnly->Sumw2();
+  hPIDOnly->Sumw2();
+  hTpcOnly->Sumw2();
+  hTpcHftTopo->Sumw2();
+  h2MassPt->Sumw2();
+
+}
+
+void pidEfficiencyMaker::Finish()
+{
+  //tbd
+}
+
+void pidEfficiencyMaker::Make() // for each LambdaC ... (not event)
+{
+  //tbd
+}
+
+bool pidEfficiencyMaker::passTPC()
+{
+  //tbd
+}
 
 
--- LcSimNtuple/pidEfficiencyMaker.h
+++ LcSimNtuple/pidEfficiencyMaker.h
@@ -5,31 +5,37 @@
 #include <TGraphErrors.h>
 #include <TF1.h>
 #include <array>
+#include <string>
+#include <TFile.h>
 
 class pidEfficiencyMaker
 {
   public:
 
-    pidEfficiencyMaker();
+    pidEfficiencyMaker(std::string outFileName, int centrality);
     ~pidEfficiencyMaker();
 
     enum PidNumbers {kPion = 0, kKaon = 1, kProton = 2};
     enum NBinsConsts {NptBins  = 9};
 
     void SetSimMaker(nt* simNTuple) 
-      {simTuple = simNTuple;}
+    {simTuple = simNTuple;}
     void SetHftRatioCorrection(TGraphErrors *hftRatioCorrection) 
-      {fHftRatioCorrection = hftRatioCorrection; }
+    {fHftRatioCorrection = hftRatioCorrection; }
     void SetAuAuWeight(TF1* f1AuAu010Weight) 
-      {gf1AuAu010Weight = f1AuAu010Weight; }
+    {gf1AuAu010Weight = f1AuAu010Weight; }
 
     void SetNsigWeight(TF1* nSigEffFunc, int pidFlag) 
-      {fNsig_eff[pidFlag] = nSigEffFunc; }
+    {fNsig_eff[pidFlag] = nSigEffFunc; }
     void SetNSigTof(TF1* nSigTofEffFunc, int pidFlag) 
-      {fNsigTof_eff[pidFlag] = nSigTofEffFunc;}
+    {fNsigTof_eff[pidFlag] = nSigTofEffFunc;}
     void SetTofEff(TF1* tofEffFunc, int ptBin, int pidFlag) 
-      {ftof[ptBin][pidFlag] = tofEffFunc;}
+    {ftof[ptBin][pidFlag] = tofEffFunc;}
 
+    void SetCentrality(int centrality) {mCentrality = centrality;}
+    void SetMinPtCut  (float minPtCut) {mMinPtCut   = minPtCut;  }
+    int  GetCentrality()  {return mCentrality;}
+    int  GetMinPtCut  ()  {return mMinPtCut;  }
 
     void Init();
     void Make();
@@ -48,6 +54,22 @@ class pidEfficiencyMaker
     array<TF1*, 3> fNsigTof_eff;
 
     array<array<TF1*, NptBins>, 3> ftof;
+
+    bool passTPC();
+
+    TH1D* hNoCuts;
+    TH1D* hNoCutsPhysBinning;
+    TH1D* hTopoCuts;
+    TH1D* hHftMatchingOnly;
+    TH1D* hPIDOnly;
+    TH1D* hTpcOnly;
+    TH1D* hTpcHftTopo;
+    TH2D* h2MassPt;
+
+    float mMinPtCut;
+    int   mCentrality;
+
+    TFile* outFile
 }
 
 #endif
